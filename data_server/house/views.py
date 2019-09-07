f3e330615fe2cd38bb94ff744ad0ef925bbe67d3
Ysfu <835340685@qq.com>
2019-09-07 16:47:46 +0800

    init

--- data_server/house/views.py
+++ data_server/house/views.py
@@ -7,9 +7,19 @@ def house_views(request):
     :param request:
     :return:
     """
-    # if request.method == "GET":
-    #     print('get')
-    #     user_message = json.loads(request.body)
-    #     print(user_message)
+    if request.method == "POST":
+        print('POST')
+        user_message = json.loads(request.body)
+        # 前端传过来的数据 {'W_L': [116.42543, 39.903909], 'DS': 8.333333333333332, 'price': '1000'}
+        # W_L: 工作地点经纬度， DS:距离 单位km  prices：接受价格1000  条件就是0<=1000
+        print(user_message)
 
-    return JsonResponse({'code':200})
+        toget_array=[
+            {'locat':[116.4254309,39.903901],
+             'house_name':'月季园',
+             'url':'www.baidu.com',
+             'message':'啥啥啥',
+            }
+        ]
+
+    return JsonResponse({'code':200,'data':toget_array})
